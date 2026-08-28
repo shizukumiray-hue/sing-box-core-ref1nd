5ee67819d0b89e7bc726d27f7cbf08370668f8d4
shizukumiray-hue <shizukumiray-hue@users.noreply.github.com>
2026-08-28 16:15:36 +0800

    fix: disable pidfd workaround for gomobile builds
    The go:linkname directive cannot be used in c-shared buildmode
    (gomobile bind). Add build constraint to exclude this file from
    Android gomobile builds.
    
    Build tag: !android || disable_pidfd_workaround

--- experimental/libbox/pidfd_android.go
+++ experimental/libbox/pidfd_android.go
@@ -1,3 +1,5 @@
+//go:build !android || disable_pidfd_workaround
+
 package libbox
 
 import (
@@ -8,6 +10,10 @@ import (
 // https://github.com/SagerNet/sing-box/issues/3233
 // https://github.com/golang/go/issues/70508
 // https://github.com/tailscale/tailscale/issues/13452
+// 
+// NOTE: This workaround is disabled for gomobile builds because
+// go:linkname cannot be used in c-shared buildmode.
+// The original issue should not affect gomobile-built libraries.
 
 //go:linkname checkPidfdOnce os.checkPidfdOnce
 var checkPidfdOnce func() error
